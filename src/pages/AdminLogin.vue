a57f780e6f97b0e042e45626ea9c12a78f97d134
Nurmuhammad <umrzoqovndev1998@gmail.com>
2024-12-11 22:40:06 +0500

    added alert

--- src/pages/AdminLogin.vue
+++ src/pages/AdminLogin.vue
@@ -1,6 +1,6 @@
 <script setup>
 import FormInput from "@/components/FormInput.vue";
-import {ref,reactive} from "vue";
+import { ref, reactive } from "vue";
 import { useAuthorization } from "@/stores/admin/authorization.js";
 import { useRouter } from "vue-router";
 
@@ -9,14 +9,24 @@ const router = useRouter();
 let authorization = reactive({
     email: '',
     password: ''
-})
+});
 
-function auth() {
+function auth(event) {
+    event.preventDefault(); 
     isLoading.value = true;
-    useAuthorization().adminAuth(authorization)
-    .then(()=>{
-        router.push('/statistics')
-    })
+
+    useAuthorization()
+        .adminAuth(authorization)
+        .then(() => {
+            router.push('/statistics');
+        })
+        .catch((error) => {
+            alert("Email yoki parol noto'g'ri!"); 
+            console.error("Auth error:", error);
+        })
+        .finally(() => {
+            isLoading.value = false;
+        });
 }
 </script>
 
@@ -24,25 +34,22 @@ function auth() {
 <div class="container-fluid row d-flex justify-content-center align-items-center">
     <div class="col-12 col-sm-8 col-md-6 col-xl-4 ms-4">
         <h1 class="text-center fw-normal">Kirish</h1>
-        <form>
+        <form @submit="auth">
              <FormInput v-model="authorization.email" props-id="email" label-name="Email" input-type="email" />
              <FormInput v-model="authorization.password" props-id="password" label-name="Parol" input-type="password" />
             <div class="text-end text-center mt-4">
-                <button type="button" @click="auth" class="form-control shadow-none bg-primary text-white">
+                <button type="submit" class="form-control shadow-none bg-primary text-white">
                     <span v-if="!isLoading" class="fs-5">Kirish</span>
-                    <span v-if="isLoading" class="spinner-border spinner-border-sm text-light"></span>
+                    <span v-else class="spinner-border spinner-border-sm text-light"></span>
                 </button>
             </div>
         </form>
-
     </div>
-
 </div>
 </template>
 
 <style scoped>
-.container-fluid{
+.container-fluid {
     height: 80vh;
 }
-
 </style>
