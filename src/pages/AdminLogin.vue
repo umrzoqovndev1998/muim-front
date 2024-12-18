f099a0866438845056a46d631ad10ce50605babe
Nurmuhammad <umrzoqovndev1998@gmail.com>
2024-12-18 12:37:12 +0500

    added icons show and hide for password

--- src/pages/AdminLogin.vue
+++ src/pages/AdminLogin.vue
@@ -6,6 +6,9 @@ import { useRouter } from "vue-router";
 
 let isLoading = ref(false);
 const router = useRouter();
+let change = ref(false);
+let password = ref("password");
+let pathImg = ref("src/assets/icons/show.png");
 let authorization = reactive({
     email: '',
     password: ''
@@ -28,6 +31,18 @@ function auth(event) {
             isLoading.value = false;
         });
 }
+function changeIcon(){
+    change.value = !change.value;
+    if(change.value){
+        pathImg.value = "src/assets/icons/hide.png";
+        password.value = "text";
+    }
+    else{
+        pathImg.value = "src/assets/icons/show.png";
+        password.value = "password" 
+    }
+
+}
 </script>
 
 <template>
@@ -36,7 +51,8 @@ function auth(event) {
         <h1 class="text-center fw-normal">Kirish</h1>
         <form @submit="auth">
              <FormInput v-model="authorization.email" props-id="email" label-name="Email" input-type="email" />
-             <FormInput v-model="authorization.password" props-id="password" label-name="Parol" input-type="password" />
+             <FormInput v-model="authorization.password" props-id="password" label-name="Parol" :input-type="password" />
+             <button v-show="authorization.password" type="button" @click="changeIcon()" class="form-btn"><img :src="pathImg" alt="" width="15px"></button>
             <div class="text-end text-center mt-4">
                 <button type="submit" class="form-control shadow-none bg-primary text-white">
                     <span v-if="!isLoading" class="fs-5">Kirish</span>
@@ -51,5 +67,24 @@ function auth(event) {
 <style scoped>
 .container-fluid {
     height: 80vh;
+}
+form{
+    position: relative;
+}
+.form-btn{
+    position: absolute;
+    right: 10px;
+    bottom: 72px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    background-color: white;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+}
+.form-btn:hover{
+    background-color: rgb(230, 229, 229);
 }
 </style>
